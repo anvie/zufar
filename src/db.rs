f89664f9bccf9adecc6a82c4e596e0e083f6da5e
robin <robin@ansvia.com>
2015-08-15 23:07:56 +0700

    more reliable commitlog

--- src/db.rs
+++ src/db.rs
@@ -4,7 +4,7 @@ extern crate test;
 use std::collections::BTreeMap;
 
 use std::io::prelude::*;
-use std::io::BufWriter;
+use std::io::{BufWriter, BufReader};
 use std::fs::File;
 use std::path::Path;
 use std::fs::OpenOptions;
@@ -13,51 +13,121 @@ use crc32::Crc32;
 
 
 pub struct Db {
+    memtable_eden: BTreeMap<u32, Vec<u8>>,
     memtable: BTreeMap<u32, Vec<u8>>,
     fstore: File
 }
 
 impl Db {
     pub fn new() -> Db {
+        
+        let file_name = "commitlog.txt";
+        
+        let mut _memtable = BTreeMap::new();
+        {
+            let path = Path::new(file_name);
+
+            if path.exists(){
+                // load data into memtable
+                info!("loading commitlog data into memtable...");
+                let mut file = BufReader::new(File::open(&path).unwrap());
+                let mut count = 0;
+                for line in file.lines().filter_map(|result| result.ok()) {
+                    //println!("line: {}", line);
+                    let s:Vec<&str> = line.split("|").collect();
+                    let version = s[0];
+                    let hash_key:u32 = s[1].parse().unwrap();
+                    let content = s[2..].join("|");
+                    debug!("  (v{}) -> k: {}, content: {}", version, hash_key, content);
+                    _memtable.insert(hash_key, content.into_bytes());
+                    count = count + 1;
+                }
+                info!("loading commitlog done. {} record(s) added.", count);
+            }
+        }
 
         let mut file = match OpenOptions::new()
                     .write(true)
                     .create(true)
                     .append(true)
-                    .open("commitlog.txt"){
+                    .open(file_name){
                         Ok(mut f) => f,
                         Err(e) => panic!("cannot open commitlog.txt. {}", e)
                     };
         
         Db {
-            memtable: BTreeMap::new(),
+            memtable_eden: BTreeMap::new(),
+            memtable: _memtable,
             fstore: file
         }
     }
     
     pub fn insert(&mut self, k:&[u8], v:&[u8]){
         let mut crc32 = Crc32::new();
-        self.memtable.insert(crc32.crc(k), v.to_vec());
+        self.memtable_eden.insert(crc32.crc(k), v.to_vec());
         self.flush();
     }
     
     pub fn get(&mut self, k:&[u8]) -> Option<&[u8]> {
         let mut crc32 = Crc32::new();
-        self.memtable.get(&crc32.crc(k)).map(|d| d.as_ref())
+        let rv = self.memtable.get(&crc32.crc(k)).map(|d| d.as_ref());
+        if rv.is_some(){
+            rv
+        }else{
+            // try search in eden 
+            self.memtable_eden.get(&crc32.crc(k)).map(|d| d.as_ref())
+        }
     }
     
     pub fn flush(&mut self){
-        let iter = self.memtable.iter();
-        for (k, v) in iter {
-            
-            println!("flushing k: {:?}, v: {:?}", k, v);
-            
-            //let mut writer = BufWriter::new(&self.fstore);
-            
-            self.fstore.write_all(format!("{}:{}\n", k, String::from_utf8(v.clone()).unwrap()).as_bytes());
+        
+        let mut to_remove:Vec<u32> = Vec::new();
+        
+        {
+            let iter = self.memtable_eden.iter();
+
+            for (k, v) in iter {
+
+                println!("flushing k: {:?}, v: {:?}", k, v);
+
+                //let mut writer = BufWriter::new(&self.fstore);
+
+                // format: [VERSION]|[KEY-HASH]|[CONTENT]
+                self.fstore.write_all(format!("1|{}|{}\n", k, String::from_utf8(v.clone()).unwrap()).as_bytes());
+
+                to_remove.push(*k);
+            }
         }
+        
         self.fstore.flush();
         self.fstore.sync_all();
+        
+        // move flushed data
+        let mut count = 0;
+        
+        {
+            let iter = self.memtable_eden.iter();
+            for (k, v) in iter {
+                self.memtable.insert(*k, v.clone());
+                count = count + 1;
+            }
+        }
+        
+        // let mut count = 0;
+        // for key in &to_remove {
+        //     // remove from eden
+        //     self.memtable_eden.remove(key);
+        //     count = count + 1;
+        // }
+        self.memtable_eden.clear();
+        
+        info!("flushed {} item(s)", count);
+        self.print_stats();
+        
+    }
+    
+    fn print_stats(&self){
+        info!("memtable records: eden: {}, old: {}", self.memtable_eden.len(), self.memtable.len());
     }
 }
 
